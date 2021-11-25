55c9774f519e943f7278a8e77f143574884f8a2b
lipet2k <56457466+lipet2k@users.noreply.github.com>
2021-11-25 12:17:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,28 +2,31 @@ from flask import Flask, render_template, url_for, request, redirect, Markup
 
 import chess
 import chess.svg
+import math
 import time
-from program import MiniMaxAgent
+from program import MiniMaxAgent, MiniMaxAlphaBetaAgent
 from enum import Enum
 
 app = Flask(__name__)
 
 board = chess.Board()
-agent = MiniMaxAgent(board)
+agent = MiniMaxAlphaBetaAgent(board)
 
 def renderBoard(value):
     new_image = chess.svg.board(board, size=500)
     return render_template("index.html", new_image = Markup(new_image), value=value)
 
 def play(depth):
-    best_action = agent.bestAction(depth)
+    best_action = agent.bestAction(depth, -math.inf, math.inf)
     best_value = best_action[0]
     best_move = best_action[1]
     if best_move is None:
-        return str(agent.color) + " wins!"
+        if agent.board.turn == False:
+            return "White wins!"
+        else:
+            return "Black wins!"
     else:
         board.push(best_move)
-        agent.swap()
         return renderBoard(best_value)
 
 @app.route('/')
@@ -34,7 +37,6 @@ def init():
 def reset():
     try: 
         board.reset()
-        agent.setColor(chess.WHITE)
         return renderBoard(0)
     except:
         return "Error when resetting"
@@ -42,7 +44,7 @@ def reset():
 @app.route('/next', methods=['GET'])
 def nextMove():
     # try:
-        return play(2)
+        return play(3)
     # except:
     #     return "Error when processing the move"
 
