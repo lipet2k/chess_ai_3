d94d042a3b492931ab5ac7497a65112c35dd7d8d
melonmizu <dylanjmmarin@gmail.com>
2021-12-09 17:08:44 -0500

    finished first draft of regression methods

--- api/regression.py
+++ api/regression.py
@@ -8,27 +8,55 @@ import math
 #Input: Board
 #Output: Moves
 
+# train it on only wins and losses
+# test it on only wins and loss - assess our success
 
+# train it on win/loss/draw
+# test it
+# output 0.4-0.6 = draw
 
-class logisti_regression:
+class logistic_regression:
 
     def __init__(self):
-        self.iteration = 100
+        self.iterations = 100
         self.learning_rate = 0.01
 
-        self.features = np.array([])
-        self.weights = np.array([])
-        self.bias_weight = 1
+        # one item = an array of feature values corresponding to one position
+        self.features_set = np.array([])
+        # one item corresponds to one item in feature set
         self.y_results = np.array([])
 
+        # one set of weights
+        self.weights = np.array([])
+        self.bias_weight = 1
 
-    def loss_function(self, y_i):
+    def regression(self):
+        for iteration in range(self.iterations):
+            self.update_weights()
+        print(self.weights)
+
+    # update all weights (not bias term) for data input
+    def update_weights(self):
+        new_weights = np.array([])
+        # for each weight
+        for j in range(self.weights.size):
+            # new weight = old weight + learning rate * derivative of loss func
+            new_weights[j] = self.weights[j] + self.learning_rate * self.gradient(self.y_results, self.features_set)
+        self.weights = new_weights
+
+    # y = set of results
+    # features is set of collections of features
+    # y[n] is the result of the position represented by features[n]
+    def gradient(self, y, features, j):
         total = 0
-        for i in range(y_i.size):
-            total += y_i[i] - total_sum(bias_weight)
+        for i in range(y.size):
+            corresponding_features = features[i]
+            total += y[i] - self.total_sum(self.bias_weight, corresponding_features, self.weights) \
+                     * corresponding_features[j]
+        return total
 
     def total_sum(self, bias, features, weights):
-        return sigmoid(bias + np.dot(features, weights))
+        return self.sigmoid(bias + np.dot(features, weights))
 
     def sigmoid(self, z):
         return 1 / (1 + math.exp(-z))
