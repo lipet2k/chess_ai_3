4fbe5e81a829fa670bbe29ef2e69228cbacf2492
Peter Li <li.peter7122@gmail.com>
2021-12-09 16:41:54 -0500

    What is this?

--- api/regression.py
+++ api/regression.py
@@ -2,13 +2,33 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from moves import pos_moves
+import math
 
 # MIRA
 #Input: Board
 #Output: Moves
-i = 0
-w_0 = 0
-pos_moves = np.array(pos_moves)
 
-for i in range(1):
-    pass
+
+
+class logisti_regression:
+
+    def __init__(self):
+        self.iteration = 100
+        self.learning_rate = 0.01
+
+        self.features = np.array([])
+        self.weights = np.array([])
+        self.bias_weight = 1
+        self.y_results = np.array([])
+
+
+    def loss_function(self, y_i):
+        total = 0
+        for i in range(y_i.size):
+            total += y_i[i] - total_sum(bias_weight)
+
+    def total_sum(self, bias, features, weights):
+        return sigmoid(bias + np.dot(features, weights))
+
+    def sigmoid(self, z):
+        return 1 / (1 + math.exp(-z))
