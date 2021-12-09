e4ef3e830168d6e51d5f7687363af651825d765c
Peter Li <li.peter7122@gmail.com>
2021-12-09 15:20:07 -0500

    Adding things

---
+++ api/regression.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from moves import pos_moves
+
+# MIRA
+#Input: Board
+#Output: Moves
+i = 0
+w_0 = 0
+pos_moves = np.array(pos_moves)
+
+for i in range()
