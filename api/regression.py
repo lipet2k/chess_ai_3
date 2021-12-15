4399b51df2ef40d9b6d31ff70e90a1a2fc0ba87f
Peter Li <li.peter7122@gmail.com>
2021-12-15 13:23:20 -0500

    Is it right?

--- api/regression.py
+++ api/regression.py
@@ -2,6 +2,7 @@ import numpy as np
 # import matplotlib.pyplot as plt
 import math
 import pandas as pd
+import random
 
 # MIRA
 #Input: Board
@@ -20,45 +21,53 @@ class logistic_regression:
     def __init__(self):
         self.learning_rate = 0.1
 
-        self.features_set = pd.read_excel("10_games_features.xlsx", "Sheet1")
-        df_winner = pd.read_excel("10_games_winner.xlsx", "Sheet1")
-        self.df_game_num = pd.read_excel("10_games_game_num.xlsx", "Sheet1")
-        self.y_results = df_winner.to_numpy()[0]
-        self.y_results = self.y_results[1:]
+        self.batch_size = 5
+
+        features_data = pd.read_excel("10_games_features.xlsx", "Sheet1")
+        df_winner_data = pd.read_excel("10_games_winner.xlsx", "Sheet1")
+        # self.df_game_num_data = pd.read_excel("10_games_game_num.xlsx", "Sheet1")
+        y_results_data = df_winner_data
+        # .to_numpy()[0]
+
+
+        something = y_results_data.columns.tolist()[1:]
+
+
+        random.Random(1234).shuffle(something)
+
+
+        self.new_list = something[:self.batch_size]
+        # print(self.new_list) 
+
+        self.features_set = features_data[self.new_list]
+        # print(type(self.features_set))
+        self.y_results = y_results_data[self.new_list]
+        # print(type(self.y_results))
+
+
 
 
         self.df_write = pd.DataFrame()
 
-        # tuple = file_to_arrays("first10kwhitewins.txt", "first10kblackwins.txt", 25)
-        # one item = an array of feature values corresponding to one position
-
-        # results = self.df.iloc[2]
-        # self.y_results = results[1:]
-        # self.y_results = self.y_results.to_numpy()
-        # print(self.y_results)
-        # self.y_results = np.array(results.to_numpy())
-        # self.y_results = self.y_results[1:]
-
         # self.total_y = self.df["results"]
 
-        self.iterations = 2
+        self.iterations = 10
 
         # one set of weights
-        self.weights = np.array([1] * (768 + 4 + 1 + 2))
+        self.weights = np.array([1.0] * (768 + 4 + 1 + 2))
 
         self.bias_weight = 1
-        print("regression obj initialized")
 
     def regression(self):
         for iteration in range(self.iterations):
             self.update_weights()
-            if iteration % 10 == 0:
-                print("iteration " + str(iteration) + "\nbias term: " + str(self.bias_weight) + str(self.weights))
+            if iteration % 4 == 0:
+                print(self.weights)
                 self.df_write[iteration] = self.weights
         self.df_write.to_excel("final_weights.xlsx", sheet_name="Sheet1")
     # update all weights (not bias term) for data input
     def update_weights(self):
-        new_weights = np.array([0] * 775)
+        new_weights = np.array([0.0] * 775)
         # for each weight
         self.bias_weight = self.bias_weight + self.learning_rate * self.derivative_loss()
 
@@ -75,28 +84,25 @@ class logistic_regression:
     def derivative_loss(self):
 
         total = 0
-        for i in range(len(self.y_results)):
-            corresponding_features = self.features_set[i]
-            total += self.y_results[i] - self.total_sum(self.bias_weight, corresponding_features, self.weights)
+        for i in self.new_list:
+            corresponding_features = self.features_set[i].to_numpy()
+            total += self.y_results[i] - self.total_sum(corresponding_features)
         return total
 
     def derivative_loss_second(self, j):
         total = 0
-        for i in range(len(self.y_results)):
-            corresponding_features = self.features_set[i]
-            total += (self.y_results[i] - self.total_sum(self.bias_weight, corresponding_features, self.weights))*self.features_set[i][j]
+        for i in self.new_list:
+            corresponding_features = self.features_set[i].to_numpy()
+            total += (self.y_results[i] - self.total_sum(corresponding_features))*corresponding_features[j]
         return total
 
-    def total_sum(self, bias, features, weights):
-        # print(type(features))
-        # print(weights)
-        # features = np.fromstring(features, dtype=int, sep=",")
-        # print(features)
-        return self.sigmoid(bias + np.dot(features, weights))
+    def total_sum(self, features):
+        return self.sigmoid(self.bias_weight + np.dot(features, self.weights))
 
     def sigmoid(self, z):
-        return 1 / (1 + math.exp(-z))
+        return 1.0 / (1.0 + math.exp(-z))
 
 
 tester = logistic_regression()
 tester.regression()
+print(tester.weights)
